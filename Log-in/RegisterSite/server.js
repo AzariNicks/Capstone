7398ed848623f3531df9855606c5497b5c241797
Azari <itsazari9321@gmail.com>
2022-05-04 22:18:41 -0400

    1st Captone

---
+++ Log-in/RegisterSite/server.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+app.use(express.json())
+app.use(cors())
+app.listen(4004, () => { console.log(`running on port 400`)})
