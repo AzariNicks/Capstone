94ef6aa4e61fba43e66246f8f60a9497398580a8
AzariNicks <azari.nicks99@gmail.com>
2022-05-09 18:46:34 -0400

    l

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.get('/js2', (req, res) => {
     cl('JavaScript For RegisterPage Sent To Server')
 })
 app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, '/emailsite/Login.html'))
+    res.sendFile(path.join(__dirname, '/emailsite/Register.html'))
      cl('HTML Log-in Page Sent To Server')
 })
 app.get('/style3', (req, res) => {
@@ -65,7 +65,7 @@ heres a cute copy paste so its easy to get to
 /*
 git init
 git add .
-git commit -m "made all folders useless"
+git commit -m "l"
 git push heroku master
 
 
