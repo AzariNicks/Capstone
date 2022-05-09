7498b81f5f0747c1e9710efb19305d7777cb8afc
AzariNicks <azari.nicks99@gmail.com>
2022-05-09 01:24:42 -0400

    switched home for register to log-in

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get('/js', (req, res) => {
     res.sendFile(path.join(__dirname, '/emailsite/main.js'))
     // cl('We got the JavaScript Through')
 })
-app.get('/', (req, res) => {
+app.get('/register', (req, res) => {
     res.sendFile(path.join(__dirname, '/Register/Register.html'))
     // cl('HTML RegisterPage Sent To Server')
 })
@@ -37,7 +37,7 @@ app.get('/js2', (req, res) => {
     res.sendFile(path.join(__dirname, '/Register/Register.js'))
     // cl('JavaScript For RegisterPage Sent To Server')
 })
-app.get('/Login', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/Login/Login.html'))
     // cl('HTML Log-in Page Sent To Server')
 })
@@ -65,6 +65,6 @@ heres a cute copy paste so its easy to get to
 
 // git init
 // git add .
-// git commit -m "CHANGED a get to have Login"
+// git commit -m "switched home for register to log-in"
 // git push heroku master
 // DONE 
