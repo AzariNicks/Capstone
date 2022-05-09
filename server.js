8acd064c629e167e8256a0d41d93df9a614ded37
AzariNicks <azari.nicks99@gmail.com>
2022-05-09 00:54:53 -0400

    AddedNewPort

--- server.js
+++ server.js
@@ -6,12 +6,13 @@ const express = require('express')
 const cors = require('cors')
 const path = require('path')
 const app = express()
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3456;
 
 
 app.use(express.json())
 app.use(cors())
 const serverNumber = 3456
+app.use(express.static(path.join(__dirname, 'capstone')))
 app.get('/email', (req, res) => {
     res.sendFile(path.join(__dirname, '/emailsite/main.html'))
     cl('We got the html through')
