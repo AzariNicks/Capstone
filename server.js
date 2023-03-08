43383f298990817fa4e82fdbace4f457d2f95f6d
Azari <itsazari9321@gmail.com>
2023-03-07 22:52:10 -0500

    first commit

--- server.js
+++ server.js
@@ -1,60 +1,38 @@
 const cl = (TheThingYouWantToConsoleLog) => {console.log(TheThingYouWantToConsoleLog)}
 const cl2 = () => { cl('hello this thing is workings')}
 
-const axios = require('axios')
+
 const express = require('express')
 const cors = require('cors')
 const path = require('path')
 const app = express()
-const PORT = process.env.PORT || 3456;
+const axios = require('axios')
+const PORT = process.env.PORT || 5000;
 
 
 app.use(express.json())
 app.use(cors())
-const serverNumber = 3456
-// app.use(express.static(path.join(__dirname, 'capstone')))
-app.get('/email', (req, res) => {
-    res.sendFile(path.join(__dirname, '/emailsite/main.html'))
-    cl('We got the html through')
-})
-app.get('/style', (req, res) => {
-    res.sendFile(path.join(__dirname, '/emailsite/main.css'))
-    // cl('We got the css through')
-})
-app.get('/js', (req, res) => {
-    res.sendFile(path.join(__dirname, '/emailsite/main.js'))
-    // cl('We got the JavaScript Through')
-})
+const serverNumber = 3500
+
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/Register/Register.html'))
+    res.sendFile(path.join(__dirname, '/code-a-webpage/main.html'))
     cl('HTML RegisterPage Sent To Server')
 })
-app.get('/style2', (req, res) => {
-    res.sendFile(path.join(__dirname, '/Register/Register.css'))
+app.get('/style', (req, res) => {
+    res.sendFile(path.join(__dirname, '/code-a-webpage/main.css'))
     cl('CSS For HtmlPage Sent To Server')
 })
-app.get('/js2', (req, res) => {
-    res.sendFile(path.join(__dirname, '/Register/Register.js'))
+app.get('/js', (req, res) => {
+    res.sendFile(path.join(__dirname, '/code-a-webpage/main.js'))
     cl('JavaScript For RegisterPage Sent To Server')
 })
-app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, '/Login/test.html'))
-     cl('HTML Log-in Page Sent To Server')
-})
-app.get('/style3', (req, res) => {
-    res.sendFile(path.join(__dirname, '/Login/Login.css'))
-    cl('Css Log-in Page Sent To Server')
-})
-app.get('/js3', (req, res) => {
-    res.sendFile(path.join(__dirname, '/Login/Login.js'))
-    cl('Javascript Log-in Page Sent To Server')
-})
-const {RegUser, getAllUserInfo, checkingPassword} = require('./controller.js')
 
-app.get('/usernames',   getAllUserInfo)
-app.post('/usernames', RegUser)
-app.post('/verfy',checkingPassword)
-app.listen(PORT, () => { cl(`running on port ${PORT}
+
+
+
+app.listen(PORT, () => { cl(
+    `running on port ${PORT}
 heres a cute copy paste so its easy to get to
  www.localhost:${PORT} `)})
 // git remote -v
